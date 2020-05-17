54dc1f9b7c3f07bf118efe2d3a3b2879aea98ed2
LAURA AC <53809569+lauraac@users.noreply.github.com>
2020-05-17 05:35:07 -0500

    DataLovers

---
+++ test/data.spec.js
@@ -0,0 +1,15 @@
+
+
+// importamos la función `example`
+import { example } from "../src/data";
+
+describe('example', () => {
+
+  it('debería ser una función', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  describe('example', () => {
+    // escribe aquí tu test
+  });
+});
